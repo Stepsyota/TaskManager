bb271253aeb49b17a5caeb6c5b4f57c24ca0cfd4
Stepan <stepsyota@gmail.com>
2024-12-08 14:32:14 +0300

    Add: Task pydantic  model

--- schemes.py
+++ schemes.py
@@ -1,8 +1,6 @@
 from pydantic import BaseModel
-from typing import Optional
 
 class TaskADD(BaseModel):
     id : int
     title : str
-    completed : bool
-    description : Optional[str] = None
+    completed : bool = False
