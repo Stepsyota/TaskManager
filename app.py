01ab1ca9642584c30f227f74e3c7893a6f42d372
Stepan <stepsyota@gmail.com>
2024-12-10 21:59:07 +0300

    Add: new design and interactive functions, link with DB

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import streamlit as st
-
-from frontend import show_task_buttons, add_task_button, apply_css
-from database import get_db, Session
-from main import create_task, get_tasks
-from schemes import TaskADD
-import pandas as pd
 import requests
 
 API_URL = 'http://127.0.0.1:8000/'
 st.title("Task Manager")
-apply_css()
-def show_tasks():
+
+def get_tasks():
     response = requests.get(f"{API_URL}/tasks")
     if response.status_code == 200:
-        show_task_buttons(response.json())
+        return response.json()
+    else:
+        st.error("Failed to fetch tasks")
+        return {}
+
+def update_task(task):
+    st.write(task)
+    response = requests.patch(f"{API_URL}/tasks", json=task)
+    if response.status_code == 200:
+        return response.json()
     else:
         st.error("Failed to fetch tasks")
 
@@ -22,10 +25,100 @@ def delete_task(task_id):
     params['task_id'] = task_id
     response = requests.delete(f"{API_URL}/tasks", params=params)
     if response.status_code == 200:
-        st.write("SUCCES")
+        return response.json()
     else:
         st.error("Failed to fetch tasks")
-show_tasks()
-add_task_button()
 
-delete_task(1200)
+def create_task(task):
+    response = requests.post(f"{API_URL}/tasks", json=task)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        st.error("Failed to fetch tasks")
+
+
+def show_add_menu():
+    st.header(f"Добавление задачи")
+    with st.form("add_form"):
+        title = st.text_input("Название задачи")
+        completed = st.checkbox("Задача выполнена")
+        submitted = st.form_submit_button("Сохранить")
+
+        if submitted:
+            st.success(f"Задача сохранена: {title}, выполнена: {completed}")
+            st.session_state.show_add_menu = False
+            new_task = {
+                'id' : 0,
+                'title' : title,
+                'completed' : completed
+            }
+            st.write(new_task)
+            create_task(new_task)
+
+def show_edit_menu(task):
+    st.header(f"Редактирование задачи")
+    with st.form("edit_form"):
+        title = st.text_input("Название задачи", value= f"{task['title']}")
+        completed = st.checkbox("Задача выполнена", value=f"{task['completed']}")
+        col1, col2, col3 = st.columns([2, 6, 2])
+        with col1:
+            submitted = st.form_submit_button("Сохранить")
+        with col3:
+            deleted = st.form_submit_button("Удалить")
+
+        if deleted:
+            delete_task(task['id'])
+        elif submitted:
+            st.success(f"Задача обновлена")
+            task['title'] = title
+            task['completed'] = completed
+            update_task(task)
+            st.session_state.show_edit_menu = False
+
+def show_task_menu(task):
+    st.subheader("Подробное описание задачи")
+    st.write("---")  # Разделительная линия
+    st.markdown(f"**Название:** {task['title']}")
+    st.markdown(f"**Выполнена:** {'Да' if task['completed'] else 'Нет'}")
+    #st.text(f"Описание задачи: {task['description']}")
+    if st.button("Закрыть", key=f"close_details_{task['id']}"):
+        st.session_state[f"show_details_{task['id']}"] = False
+    st.write("---")  # Разделительная линия
+
+def show_all_tasks(tasks):
+    for task in tasks:
+        # Используем контейнер для каждой задачи
+        with st.container(border=True):
+            # Настраиваем разметку с колонками
+            col1, col2, col3 = st.columns([8, 1, 1])  # Название задачи, кнопка, чекбокс
+
+            with col1: # Название
+                if st.button(task["title"], key=f"title_{task['id']}", use_container_width=True):  # Название задачи - интерактивная кнопка
+                    # Переключаем состояние меню описания задачи
+                    current_state = st.session_state.get(f"show_details_{task['id']}", False)
+                    st.session_state[f"show_details_{task['id']}"] = not current_state
+
+            with col2: # "Редактировать"
+                if st.button("✏️", key=f"edit_{task['id']}"):
+                    current_state = st.session_state.get(f"show_edit_{task['id']}", False)
+                    st.session_state[f"show_edit_{task['id']}"] = not current_state
+
+            with col3: # Чекбокс
+                completed = st.checkbox("", value=task["completed"], key=f"chk_{task['id']}", disabled=True)
+                if completed != task["completed"]:
+                    task["completed"] = completed
+                    # Здесь можно добавить вызов API для обновления статуса на сервере
+
+        if st.session_state.get(f"show_details_{task['id']}", False):
+            show_task_menu(task)
+        if st.session_state.get(f"show_edit_{task['id']}", False):
+            show_edit_menu(task)
+
+    if st.button("Add Task", use_container_width=True):
+        current_state = st.session_state.get(f"show_add_menu", False)
+        st.session_state[f"show_add_menu"] = not current_state
+
+    if st.session_state.get(f"show_add_menu", False):
+        show_add_menu()
+
+show_all_tasks(get_tasks())
